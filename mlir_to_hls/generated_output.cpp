4a96624e3af988fb6d0c6d993825d1f0e9d14f45
Sunwoo Kim <sunwookim028@gmail.com>
2025-11-18 20:32:24 -0500

    Clean up: remove old output files, keep only latest with static inside function

--- mlir_to_hls/generated_output.cpp
+++ mlir_to_hls/generated_output.cpp
@@ -13,15 +13,14 @@
 #include <math.h>
 #include <stdint.h>
 using namespace std;
-static int32_t acc_stateful_5700927378943735518 = {0};	// L2
 void test(
   int32_t v0,
   int32_t *v1
 ) {	// L3
+  static int32_t acc_stateful_5700927378943735518 = {1};	// L2
   // placeholder for const int32_t acc_stateful_5700927378943735518	// L4
   int32_t v3 = acc_stateful_5700927378943735518;	// L5
-  int32_t v4 = v3 + v0;	// L6
-  acc_stateful_5700927378943735518 = v4;	// L7
-  *v1 = acc_stateful_5700927378943735518;	// L8
+  *v1 = v3 + v0;	// L6
+  acc_stateful_5700927378943735518 = *v1;	// L7
 }
 
