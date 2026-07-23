80bf5906c8c89d88e4a4678467f65bbeffb2375d
jb2698 <jb2698@cornell.edu>
2026-07-23 11:34:19 -0400

    adding allo rebuild+testfiles

--- allo/backend/asic/designs/TestNPU-tile/rtl/sv2v_manifest.f
+++ allo/backend/asic/designs/TestNPU-tile/rtl/sv2v_manifest.f
@@ -1,6 +1,7 @@
 # Packages
 npu/npu_config_pkg.sv
 npu/npu_isa_pkg.sv
+compute_tile/tile_agu_pkg.sv
 
 # Source directories, expanded by sv2v-design-collector
 common/
@@ -8,4 +9,4 @@ compute_tile/fpu/
 compute_tile/
 
 # Excludes
-!compute_tile/systolic_tb_wrap.sv 
+!compute_tile/systolic_tb_wrap.sv
